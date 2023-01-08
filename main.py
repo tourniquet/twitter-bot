3f93dcca0a55406126fabc59556cfea1b1985ff5
tourniquet <admyn3d@gmail.com>
2023-01-08 21:15:31 +0000

    tweet every time when the time is 9:31

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 import tweepy
+from datetime import datetime
+import time
+
 from keys import api_key, api_secret, access_token, access_token_secret
 
-def api():
-  auth = tweepy.OAuthHandler(api_key, api_secret)
-  auth.set_access_token(access_token, access_token_secret)
-
-  return tweepy.API(auth)
+auth = tweepy.OAuth1UserHandler(
+  api_key, api_secret, access_token, access_token_secret
+)
 
 
 def tweet(api: tweepy.API, message: str):
   api.update_status(message)
 
 
-if __name__ == '__main__':
-  api = api()
-  tweet(api, 'This was tweeted from Python')
+while True:
+  api = tweepy.API(auth)
+
+  print(datetime.now().strftime('%I:%M %p, %A, %B %d, %Y'))
+
+  if datetime.now().strftime('%I:%M') == '9:31':
+    tweet(api, datetime.now().strftime('%I:%M, %A, %B %d, %Y'))
+
+  time.sleep(60)
